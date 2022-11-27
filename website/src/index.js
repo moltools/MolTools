44c1d578068d1909360be8d9a67529562466368b
David Meijer <david-meijer@live.nl>
2022-11-28 00:03:12 +0100

    ENH: added tile layout apps

---
+++ website/src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import CineMol from "./pages/CineMol";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cinemol" element={<CineMol />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ website/src/pages/NoPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoPage() {
+    return (
+        <div className="no-page">
+            This page does not exist yet
+        </div>
+    );
+};
+
+export default NoPage;
